b28423a76f012981928d06969851cfbad29de0f1
Sergio Rodriguez <ingensiscomescom@outlook.com>
2021-04-26 01:49:52 -0500

    Se agregaron los algoritmos de ordenamiento y algunos calculos

--- b_lineal.sh
+++ b_lineal.sh
@@ -1,19 +1,68 @@
 #!/bin/bash
-buscar=(322486 14700764 3128036 6337399 61396 10393545 2147445644 1295390003 450057883 187645041 1980098116 152503 5000 1493283650 214826 1843349527 1360839354 2109248666 2147470852 10000000)
-
+buscando=(322486, 14700764, 3128036, 6337399, 61396, 10393545, 2147445644, 1295390003, 450057883, 187645041, 1980098116, 152503, 5000, 1493283650, 214826, 1843349527, 1360839354, 2109248666, 2147470852, 0)
 numeros=(1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000)
 
 clear
+rm -r resultado/*.txt
 
-gcc busqueda.c algoritmos_b.c general.c -o bLineal
+gcc bLinFib/busqueda.c bLinFib/algoritmos_b.c bLinFib/general.c -lpthread -o bFibo
 
 for (( j=0; j<${#numeros[@]}; j++ )) # ${#numeros[@]}
 do
-    for (( i=0; i<${#buscar[@]}; i++ ))
+    echo "Iterando con n = " ${numeros[$j]} >>resultado/fibonacci.txt
+    for (( i=0; i<${#buscando[@]}; i++ )) # ${#buscando[@]}
     do
-        echo "iterando con n = " ${numeros[$j]}
-        ./bLineal ${numeros[$j]} ${buscar[$i]} 8 
-        echo 
+        
+        ./bFibo ${numeros[$j]} ${buscando[$i]} 8 >>resultado/fibonacci.txt
+        echo >> resultado/fibonacci.txt
     done
 done
+
+#for (( j=0; j<${#numeros[@]}; j++ )) # ${#numeros[@]}
+#do
+#    echo "Iterando con n = " ${numeros[$j]} >>resultado/fibonacci.txt
+#    for (( i=0; i<3; i++ )) # ${#buscando[@]}
+#    do
+        
+#        ./bLineal ${numeros[$j]} ${buscando[$i]} 8 >>resultado/fibonacci.txt
+#        echo >> resultado/fibonacci.txt
+#    done
+#done
+
+#for (( j=0; j<${#numeros[@]}; j++ )) # ${#numeros[@]}
+#do
+#    echo "Iterando con n = " ${numeros[$j]} >>resultado/exponencial.txt
+#    for (( i=0; i<3; i++ )) # ${#buscando[@]}
+#    do
+#        
+#        ./bLineal ${numeros[$j]} ${buscando[$i]} 8 >>resultado/exponencial.txt
+#        echo >> resultado/exponencial.txt
+#    done
+#done
+
+#gcc ABB/TestArBin.c ABB/general.c -o bABB
+
+#for (( j=6; j<${#numeros[@]}; j++ )) # ${#numeros[@]}
+#do
+#    echo "Iterando con n = " ${numeros[$j]} >>resultado/ABB.txt
+#    for (( i=0; i<${#buscando[@]}; i++ )) # ${#buscando[@]}
+#    do
+        
+#        ./bABB ${numeros[$j]} ${buscando[$i]} 8 >>resultado/ABB.txt
+#        echo >> resultado/ABB.txt
+#    done
+#done
+
+#gcc bBin/testBusquedaBinaria.c bBin/busquedaBinaria.c -o bBinaria
+
+#for (( j=0; j<${#numeros[@]}; j++ )) # ${#numeros[@]}
+#do
+#    echo "Iterando con n = " ${numeros[$j]} #>>resultado/binaria.txt
+#    for (( i=0; i<3; i++ )) # ${#buscando[@]}
+#    do
+        
+#        ./bBinaria ${numeros[$j]} ${buscando[$i]} 8 #>>resultado/binaria.txt
+#        echo #>> binaria.txt
+#    done
+#done
 
